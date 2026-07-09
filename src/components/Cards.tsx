4b4ab5228f401c4e66237ef0e1e93eb0c479f6a9
Kevani Minott <kevaniminott047@gmail.com>
2026-07-08 23:50:40 -0500

    fixed the layout for the cards

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -1,3 +1,28 @@
 export function Cards() {
-  return <main></main>;
+  return (
+    <main className="w-full h-full flex  mt-[50px] gap-[40px] justify-center ">
+      <div className=" h-full flex flex-col items-center gap-[30px]">
+        <article className="w-full h-[230px] shrink-0 rounded-3xl bg-gray-700/60"></article>
+        <div className="w-full h-ful flex flex-col gap-[30px] justify-center items-center">
+          <section className="flex gap-7">
+            <div className="w-[160px] rounded-[8px] h-[100px] bg-white"></div>
+            <div className="w-[160px] rounded-[8px] h-[100px] bg-white"></div>
+            <div className="w-[160px] rounded-[8px] h-[100px] bg-white"></div>
+            <div className="w-[160px] rounded-[8px] h-[100px] bg-white"></div>
+          </section>
+
+          <section className="flex  gap-7 ">
+            <div className="w-[80px] rounded-[8px] h-[130px] bg-white"></div>
+            <div className="w-[80px] rounded-[8px] h-[130px] bg-white"></div>
+            <div className="w-[80px] rounded-[8px] h-[130px] bg-white"></div>
+            <div className="w-[80px] rounded-[8px] h-[130px] bg-white"></div>
+            <div className="w-[80px] rounded-[8px] h-[130px] bg-white"></div>
+            <div className="w-[80px] rounded-[8px] h-[130px] bg-white"></div>
+            <div className="w-[80px] rounded-[8px] h-[130px] bg-white"></div>
+          </section>
+        </div>
+      </div>
+      <div className="bg-amber-300 w-[280px] h-[530px]"></div>
+    </main>
+  );
 }
--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,9 +1,10 @@
+import { Cards } from './Cards';
 import { Search } from './Search';
 
 export function Dashboard() {
   return (
-    <main className="w-screen h-screen bg-[rgb(1,5,44)] fixed ">
-      <h2 className="text-white font-sans ml-[100px] mt-[50px] font-semibold text-[25px]">
+    <main className="w-screen h-screen bg-[rgb(1,5,44)] fixed overflow-scroll ">
+      <h2 className="text-white font-sans ml-[100px] font-semibold text-[25px]">
         Weather App
       </h2>
       <section className="flex flex-col items-center w-full h-full">
@@ -11,6 +12,7 @@ export function Dashboard() {
           How's the weather looking today?{' '}
         </h2>
         <Search />
+        <Cards />
       </section>
     </main>
   );
