bde986b34d2091789333e4bd147c3416b0f2efc8
Kevani Minott <kevaniminott047@gmail.com>
2026-07-09 22:56:38 -0500

    made the search feature work.

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,23 +1,28 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { Cards } from './Cards';
 import { Search } from './Search';
 
 export function Dashboard() {
+  const [query, setQuery] = useState<string>();
   useEffect(() => {
     fetch(
-      'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={3cd9399c66c69e503544aee4c990e556}&units=metric',
-    );
-  }, []);
+      `https://api.openweathermap.org/data/2.5/weather?q=${query},JM&appid=${import.meta.env.VITE_API_KEY}&units=metric`,
+    )
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        if (!query) return;
+        console.log(data);
+      });
+  }, [query]);
   return (
     <main className="w-screen h-screen bg-[rgb(3,1,45)] fixed overflow-scroll ">
       <h2 className="text-white font-sans ml-[100px] font-semibold text-[25px]">
         Weather App
       </h2>
       <section className="flex flex-col items-center w-full h-full">
-        <h2 className="text-white font-sans text-[40px] font-semibold ">
-          How's the weather looking today?{' '}
-        </h2>
-        <Search />
+        <Search setQuery={setQuery} />
         <Cards />
       </section>
     </main>
--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,12 +1,25 @@
-export function Search() {
+import { useRef } from 'react';
+
+interface SearchProp {
+  setQuery: (query: string) => void;
+}
+export function Search({ setQuery }: SearchProp) {
+  const searchRef = useRef<HTMLInputElement>(null);
+  function search() {
+    if (searchRef.current) setQuery(searchRef.current?.value);
+  }
   return (
     <main className="flex w-full justify-center  items-center gap-[20px] mt-[30px]">
       <input
-        placeholder="Search for a place."
+        ref={searchRef}
+        placeholder="Search for a place. eg: Kingston,JM"
         type="text"
         className="h-[40px] pl-[20px] rounded-[8px] placeholder:text-white/70 text-white w-[450px]  outline-none bg-[rgb(39,37,65)]  "
       />
-      <button className="h-[40px] w-[100px] cursor-pointer rounded-[8px] bg-[rgb(68,85,218)] text-white">
+      <button
+        onClick={search}
+        className="h-[40px] w-[100px] cursor-pointer rounded-[8px] bg-[rgb(68,85,218)] text-white"
+      >
         search
       </button>
     </main>
